2ae0d6a544628d2f90db9d45c80a19aeb74699ed
Adnan Saeed <adnansaeed.datascientist@gmail.com>
2026-02-07 21:10:24 +0500

    use google cloud api key

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-from langchain_openai import ChatOpenAI
-from pydantic import BaseModel, Field
-from typing import TypedDict, Annotated, List
-from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, AnyMessage
+from google import genai
+import time
+import sys
 from dotenv import load_dotenv
-from langgraph.graph import START, StateGraph, END
-from langgraph.checkpoint.postgres import PostgresSaver
-from langgraph.store.postgres import PostgresStore
-from langgraph.graph.message import add_messages
-from CONFIG import OPENAI_MODEL, GROQ_MODEL
-from langchain_groq import ChatGroq
 
 load_dotenv()
 
-# llm = ChatOpenAI(model=OPENAI_MODEL)
-llm = ChatGroq(model=GROQ_MODEL)
+client = genai.Client()
 
-class state_class(TypedDict):
-    messages: Annotated[list[AnyMessage], add_messages]
-
-class task(BaseModel):
-    id: int
-    title: str = Field(description="add title of the topic")
-    brief: str = Field(description='add brielf discussion about this particular topic')
-class plan(BaseModel):
-    blog_str: str = Field(description="add text of blog")
-    tasks: List[task] = Field(description="add list of tasks", default_factory=list)
-
+def generate_text(prompt):
+    response = client.models.generate_content_stream(model="gemini-2.5-flash",contents=prompt)
+    for chunk in response:
+        if chunk.text:
+            for char in chunk.text:
+                sys.stdout.write(char)
+                sys.stdout.flush()
+                time.sleep(0.005)
+                
+generate_text("write 5 lines of paragraph blog on AI Agnet")
